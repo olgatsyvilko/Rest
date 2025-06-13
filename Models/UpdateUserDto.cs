54a20aef1554235af56d2cd63002f3259252388a
tsyvilkoolga <olgatsyvilko@coherentsolutions.com>
2025-06-13 14:11:31 +0300

    Task_50. Add UpdateUserDto model. Write tests to validate put/patch methods fot Update User functionality.

---
+++ Models/UpdateUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Rest.Models
+{
+    public class UpdateUserDto
+    {
+        [JsonPropertyName("userNewValues")]
+        public UserDto UserNewValues { get; init; }
+
+        [JsonPropertyName("userToChange")]
+        public UserDto UserToChange { get; init; }
+    }
+}
